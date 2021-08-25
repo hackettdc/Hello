a8a6bec64a41d9e3d48178c591372935b9dc23f7
hackettdc <89536600+hackettdc@users.noreply.github.com>
2021-08-25 12:04:30 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/********************************************************************************
+Hello
+*********************************************************************************
+* DaJuan Hackett
+* 09/25/21
+* CMSC255 004
+*********************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Project Description>");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Section Number>");
+		System.out.println();
+	}
+}
